d620b46379dd37fcaa22cc5ee4f8d1a9f41bc620
Tolikg63 <ttserunyan@inbox.ru>
2024-04-06 17:09:11 +0400

    Routing works sucesfully

--- crypto_shop/urls.py
+++ crypto_shop/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.Index.as_view(), name='index')
+    path('', views.Index.as_view(), name='index'),
+    path('how/', views.How.as_view(), name='how'),
+    path('about/', views.About.as_view(), name='about'),
 ]
--- crypto_shop/views.py
+++ crypto_shop/views.py
@@ -6,3 +6,13 @@ from django.views import View
 class Index(View):
     def get(self, request: HttpRequest) -> HttpResponse:
         return render(request, 'crypto_shop/index.html')
+
+
+class How(View):
+    def get(self, request: HttpRequest) -> HttpResponse:
+        return render(request, 'crypto_shop/how.html')
+    
+
+class About(View):
+    def get(self, request: HttpRequest) -> HttpResponse:
+        return render(request, 'crypto_shop/about.html')
