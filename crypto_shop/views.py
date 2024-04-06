2b58215307d22dd0b97b6b938e83a2d75104202a
Tolikg63 <ttserunyan@inbox.ru>
2024-04-06 16:48:02 +0400

    Web page is working on django

--- crypto_shop/views.py
+++ crypto_shop/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse
+from django.views import View
 
-# Create your views here.
+
+class Index(View):
+    def get(self, request: HttpRequest) -> HttpResponse:
+        return render(request, 'crypto_shop/index.html')
