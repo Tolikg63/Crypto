bfb2ec3dfd3916b8f251f80ed4983216bfd8d90d
Tolikg63 <ttserunyan@inbox.ru>
2024-04-06 17:40:24 +0400

    About section loop problem not yet solved

--- crypto_shop/views.py
+++ crypto_shop/views.py
@@ -15,4 +15,14 @@ class How(View):
 
 class About(View):
     def get(self, request: HttpRequest) -> HttpResponse:
-        return render(request, 'crypto_shop/about.html')
+        links = [
+            ('link1', "Link 1"),
+            ('link2', "Link 2"),
+            ('link3', "Link 3"),
+            ('link4', "Link 4"),
+            ('link5', "Link 5"),
+        ]
+        context = {
+            'links': links
+        }
+        return render(request, 'crypto_shop/about.html', context=context)
