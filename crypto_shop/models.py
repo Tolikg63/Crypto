5449253a96efa4d33dcbc236799cd51daaa21c3b
Tolikg63 <ttserunyan@inbox.ru>
2024-04-07 12:37:17 +0400

    Admin panel changes

--- crypto_shop/models.py
+++ crypto_shop/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class Reviews(models.Model):
+    name = models.CharField(max_length=50)
+    proffession = models.CharField(max_length=50)
+    comment = models.TextField()
